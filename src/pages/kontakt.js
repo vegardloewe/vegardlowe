41d89476fbd1f39f835562ff83aac20fc6d84b8d
vegardloewe <vegardloewe96@gmail.com>
2020-10-18 10:29:03 +0200

    commit

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
-import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -85,15 +84,6 @@ const indexQuery = graphql`
     site {
       siteMetadata {
         title
-      }
-    }
-    benchAccounting: file(
-      relativePath: { eq: "bench-accounting-49909-unsplash.jpg" }
-    ) {
-      childImageSharp {
-        fluid(maxWidth: 1360) {
-          ...GatsbyImageSharpFluid
-        }
       }
     }
   }
