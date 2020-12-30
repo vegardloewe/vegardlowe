38262ca43aae7abc739e5bbaac58f9308df1b9d6
Vegard <vegard@lowedesign.no>
2020-12-30 21:27:49 +0100

    commit

--- src/components/down.js
+++ src/components/down.js
@@ -1,30 +1,33 @@
-import React, { useEffect, createRef } from "react";
+import React, { Component } from "react";
 
 import Lottie from "lottie-web";
-import animation from "./animations/down.json";
+import animationData from "./animations/down.json";
 import './down.css'
 
 
-const Down = () => {
-  let animationContainer = createRef();
+class Down extends Component {
 
-      const defaultOptions = {
-            loop: true,
-            autoplay: true,
-            animationData: animation,
-            rendererSettings: {
-              preserveAspectRatio: 'xMidYMid slice'
-            }
-          };
 
-  return (
-    <div className="App">
-    <Lottie options={defaultOptions}
-          height={400}
-          width={400}
-    />
-    </div>
-  );
-};
+  render(){
 
-export default Down;
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return(
+      <div>
+        <Lottie options={defaultOptions}
+              height={200}
+              width={200}
+        />
+      </div>
+    )
+  }
+}
+
+export default Down
